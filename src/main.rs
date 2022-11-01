67f1748c032658edb2f1521808b1c63131f4e33d
Mark Wainwright <wainwrightml@gmail.com>
2022-11-01 08:16:21 +0000

    Use contexts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use std::{borrow::BorrowMut, ops::Add};
-
 use geo::{prelude::*, MultiPoint, Point, Polygon};
-use rand::Rng;
 use itertools::{self, Itertools};
-use sycamore::{
-    prelude::*,
-    rt::Event,
-    web::html::{path, svg},
-};
+
+use sycamore::{prelude::*, rt::Event};
 use wasm_bindgen::__rt::IntoJsResult;
-use web_sys::{MouseEvent, console::log};
+use web_sys::MouseEvent;
 
 pub const CIRCLERADIUS: f64 = 10.0;
 
@@ -22,114 +16,140 @@ fn main() {
 
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
-    let concavity = create_signal(cx, 0.1);
-    let k_nearest_points = create_signal(cx, 4.0);
-    let show_concave_hull = create_signal(cx, true);
-    let show_k_nearest_concave_hull = create_signal(cx, true);
-    let show_convex_hull = create_signal(cx, true);
-    let points = create_signal(cx, MultiPoint::<f64>::new(vec![]));
+    provide_context_ref(cx, create_signal(cx, MultiPoint::<f64>::new(vec![])));
+
+    provide_context_ref(cx, create_signal(cx, ConvexHullSettings { show: true }));
+    provide_context_ref(
+        cx,
+        create_signal(
+            cx,
+            ConcaveHullSettings {
+                show: true,
+                concavity: 0.1,
+            },
+        ),
+    );
+    provide_context_ref(
+        cx,
+        create_signal(
+            cx,
+            KNearestConcaveHullSettings {
+                show: true,
+                points: 4,
+            },
+        ),
+    );
 
     view! {cx,
-        div(style="display: flex; flex-direction: column;"){
-            SVG (points = points, concavity= concavity, show_concave_hull= show_concave_hull, show_convex_hull=show_convex_hull, show_k_nearest_concave_hull=show_k_nearest_concave_hull, k_nearest_points=k_nearest_points)
-            ConcaveHullSettings(concavity= concavity, show_concave_hull=show_concave_hull)
-            ConvexHullSettings(show_convex_hull=show_convex_hull)
-            KNearestConcaveHullSettings(show_k_nearest_concave_hull=show_k_nearest_concave_hull, points=k_nearest_points)
-            ClearButton(points=points)
+        div(){
+            SVG ()
+            ConcaveHullControl()
+            ConvexHullControl{}
+            KNearestConcaveHullControl{}
+            ClearButton{}
         }
-        
+
     }
 }
 
-
-#[component(inline_props)]
-fn SVG<'a, G: Html>(
-    cx: Scope<'a>,
-    points: &'a Signal<MultiPoint<f64>>,
-    concavity: &'a ReadSignal<f64>,
-    k_nearest_points: &'a ReadSignal<f64>,
-    show_concave_hull: &'a ReadSignal<bool>,
-    show_k_nearest_concave_hull: &'a ReadSignal<bool>,
-    show_convex_hull: &'a ReadSignal<bool>,
-) -> View<G> {  
-    
+#[component()]
+fn SVG<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let points = use_context::<Signal<MultiPoint<f64>>>(cx);
     return view! { cx,
-        
-        svg(height="500", 
-        width="500", 
+
+        svg(height="500",
+        width="500",
         style="background: lightblue;",
-        xmlns="http://www.w3.org/2000/svg", 
+        xmlns="http://www.w3.org/2000/svg",
         on:click=|ev: Event| add_or_remove_point(points, get_point(ev))  ) {
 
             (View::new_fragment(
                 points.get().0.iter().map(|&p| view! { cx, circle(cx=p.x(), cy=p.y(), r= {CIRCLERADIUS} ) }).collect()
             ))
 
-            K_Nearest_Concave_Hull_Path(points=points, show=show_k_nearest_concave_hull, k_nearest=k_nearest_points)
-            Concave_Hull_Path(points=points, concavity=concavity, show=show_concave_hull)
-            Convex_Hull_Path(points=points, show=show_convex_hull)
-            
+            K_Nearest_Concave_Hull_Path{}
+            Concave_Hull_Path()
+            Convex_Hull_Path{}
+
         }
     };
 }
 
-#[component(inline_props)]
-fn Concave_Hull_Path<'a, G: Html>(
-    cx: Scope<'a>,
-    points: &'a Signal<MultiPoint<f64>>,
-    concavity: &'a ReadSignal<f64>,
-    show: &'a ReadSignal<bool>,
-) -> View<G> {
-    view![cx, 
-    (if *show.get() && points.get().0.len() >= 3 {
-        let hull = points.get().concave_hull(*concavity.get());
-        let path_string = get_path_string(&hull);
-        view![cx,path(d=path_string, fill="transparent", stroke="blue",  )]
-    } else {
-        view![cx,]
-    })]
+#[component()]
+fn Concave_Hull_Path<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let settings = use_context::<Signal<ConcaveHullSettings>>(cx);
+    let points = use_context::<Signal<MultiPoint<f64>>>(cx);
+    view![
+        cx,
+        (if settings.get().show && points.get().0.len() >= 3 {
+            let hull = points.get().concave_hull(settings.get().concavity);
+            let path_string = get_path_string(&hull);
+            view![
+                cx,
+                path(d = path_string, fill = "transparent", stroke = "blue",)
+            ]
+        } else {
+            view![cx,]
+        })
+    ]
 }
 
-#[component(inline_props)]
-fn K_Nearest_Concave_Hull_Path<'a, G: Html>(
-    cx: Scope<'a>,
-    points: &'a Signal<MultiPoint<f64>>,
-    k_nearest: &'a ReadSignal<f64>,
-    show: &'a ReadSignal<bool>,
-) -> View<G> {
-    view![cx, 
-    (if *show.get() && points.get().0.len() >= 3 {
-        let hull = points.get().k_nearest_concave_hull(k_nearest.get().floor() as u32);
-        let path_string = get_path_string(&hull);
-        view![cx,path(d=path_string, fill="transparent", stroke="red"  )]
-    } else {
-        view![cx,]
-    })]
+#[component]
+fn K_Nearest_Concave_Hull_Path<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    log::info!("K Nearest Path");
+    let settings = use_context::<Signal<KNearestConcaveHullSettings>>(cx);
+    let points = use_context::<Signal<MultiPoint<f64>>>(cx);
+    settings.track();
+    view![
+        cx,
+        (if settings.get().show && points.get().0.len() >= 3 {
+            let hull = points.get().k_nearest_concave_hull(settings.get().points);
+            let path_string = get_path_string(&hull);
+            view![
+                cx,
+                path(d = path_string, fill = "transparent", stroke = "red")
+            ]
+        } else {
+            view![cx,]
+        })
+    ]
 }
 
-#[component(inline_props)]
-fn Convex_Hull_Path<'a, G: Html>(
-    cx: Scope<'a>,
-    points: &'a Signal<MultiPoint<f64>>,
-    show: &'a ReadSignal<bool>,
-) -> View<G> {
-    view![cx, 
-    (if *show.get() && points.get().0.len() >= 3 {
-        let hull = points.get().convex_hull();
-        let path_string = get_path_string(&hull);
-        view![cx,path(d=path_string, fill="transparent", stroke="green", stroke-dasharray="4" )]
-    } else {
-        view![cx,]
-    })]
+#[component()]
+fn Convex_Hull_Path<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let points = use_context::<Signal<MultiPoint<f64>>>(cx);
+
+    let settings = use_context::<Signal<ConvexHullSettings>>(cx);
+
+    view![
+        cx,
+        (if settings.get().show && points.get().0.len() >= 3 {
+            let hull = points.get().convex_hull();
+            let path_string = get_path_string(&hull);
+            view![
+                cx,
+                path(
+                    d = path_string,
+                    fill = "transparent",
+                    stroke = "green",
+                    stroke - dasharray = "4"
+                )
+            ]
+        } else {
+            view![cx,]
+        })
+    ]
 }
 
-fn add_or_remove_point(points: &Signal<MultiPoint>, point: Point ){
-
+fn add_or_remove_point(points: &Signal<MultiPoint>, point: Point) {
     let mut modify = points.modify();
-    if let Some((index, _)) = modify.0.iter().find_position(|x| x.euclidean_distance(&point) < CIRCLERADIUS){
-        modify.0.remove(index);        
-    }   
-    else{
+    if let Some((index, _)) = modify
+        .0
+        .iter()
+        .find_position(|x| x.euclidean_distance(&point) < CIRCLERADIUS)
+    {
+        modify.0.remove(index);
+    } else {
         modify.0.push(point)
     }
 }
@@ -139,7 +159,6 @@ fn get_point(event: Event) -> Point {
 
     Point::new(me.x() as f64, me.y() as f64)
 }
-
 
 fn get_path_string(polygon: &Polygon) -> String {
     let mut iter = polygon.coords_iter();
@@ -153,56 +172,94 @@ fn get_path_string(polygon: &Polygon) -> String {
     s
 }
 
+#[component()]
+fn ConcaveHullControl<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let settings = use_context::<Signal<ConcaveHullSettings>>(cx);
 
-#[component(inline_props)]
-fn ConcaveHullSettings<'a, G: Html>(cx: Scope<'a>, concavity: &'a Signal<f64>, show_concave_hull: &'a Signal<bool>) -> View<G> {
+    let show_signal = create_signal(cx, settings.get().show);
+    create_effect(cx, || {
+        settings.modify().show = *show_signal.get();
+    });
+
+    let concavity_signal = create_signal(cx, settings.get().concavity);
+    create_effect(cx, || settings.modify().concavity = *concavity_signal.get());
+
     view! { cx,
         div(){
             label(){"Concave Hull"}
-            input(type="checkbox", bind:checked=show_concave_hull)
-            input(type="range", min="0", step="0.001", max="1", bind:valueAsNumber=concavity) {}
-            input(type="number", min="0", step="0.001", max="1", bind:valueAsNumber=concavity) {}
+            input(type="checkbox", bind:checked=show_signal)
+            input(type="range", min="0", step="0.001", max="1", bind:valueAsNumber=concavity_signal) {}
+            input(type="number", min="0", step="0.001", max="1", bind:valueAsNumber=concavity_signal) {}
         }
-        
+
     }
 }
 
-#[component(inline_props)]
-fn ConvexHullSettings<'a, G: Html>(cx: Scope<'a>,  show_convex_hull: &'a Signal<bool>) -> View<G> {
+#[component()]
+fn ConvexHullControl<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let settings = use_context::<Signal<ConvexHullSettings>>(cx);
+
+    let show_signal = create_signal(cx, settings.get().show);
+    create_effect(cx, || settings.modify().show = *show_signal.get());
+
     view! { cx,
         div(){
             label(){"Convex Hull"}
-            input(type="checkbox", bind:checked=show_convex_hull)
-            
+            input(type="checkbox", bind:checked=show_signal)
         }
-        
     }
 }
 
-#[component(inline_props)]
-fn KNearestConcaveHullSettings<'a, G: Html>(cx: Scope<'a>, points: &'a Signal<f64>, show_k_nearest_concave_hull: &'a Signal<bool>) -> View<G> {
+#[component()]
+fn KNearestConcaveHullControl<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let settings = use_context::<Signal<KNearestConcaveHullSettings>>(cx);
+
+    let show_signal = create_signal(cx, settings.get().show);
+    create_effect(cx, || {
+        settings.modify().show = *show_signal.get();
+    });
+
+    let points_signal = create_signal(cx, settings.get().points.into());
+    create_effect(cx, || {
+        settings.modify().points = *points_signal.get() as u32
+    });
+
     view! { cx,
         div(){
             label(){"K Nearest Concave Hull"}
-            input(type="checkbox", bind:checked=show_k_nearest_concave_hull)
-            input(type="range", min="1", step="1", max="10", bind:valueAsNumber=points) {}
-            input(type="number", min="1", step="1", max="10", bind:valueAsNumber=points) {}
+            input(type="checkbox", bind:checked=show_signal)
+            input(type="range", min="1", step="1", max="10", bind:valueAsNumber=points_signal) {}
+            input(type="number", min="1", step="1", max="10", bind:valueAsNumber=points_signal) {}
         }
-        
+
     }
 }
 
-#[component(inline_props)]
-fn ClearButton<'a, G: Html>(
-    cx: Scope<'a>,
-    points: &'a Signal<MultiPoint<f64>>,
-) -> View<G> {
-    
+#[component()]
+fn ClearButton<'a, G: Html>(cx: Scope<'a>) -> View<G> {
+    let points = use_context::<Signal<MultiPoint<f64>>>(cx);
+
     return view! { cx,
         button(
-        on:click=|_|  {points.modify().0.clear()}  ) 
+        on:click=|_|  {points.modify().0.clear()}  )
         {
             "Clear All Points"
         }
     };
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ConvexHullSettings {
+    pub show: bool,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct KNearestConcaveHullSettings {
+    pub show: bool,
+    pub points: u32,
+}
+#[derive(Clone, Debug, PartialEq)]
+pub struct ConcaveHullSettings {
+    pub show: bool,
+    pub concavity: f64,
 }
